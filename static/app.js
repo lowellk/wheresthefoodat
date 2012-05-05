1feffe34adf83df47d7fce888fcfefdb9899984e
Lowell Kirsh <lowellk@gmail.com>
2012-05-04 20:50:42 -0700

    reduce global vars; better styling; add addresses; reduce warnings

--- static/app.js
+++ static/app.js
@@ -1,170 +1,173 @@
+var app = (function () {
 
-var SORT_TYPE = {
-  distance: 1
-};
-
-var MAP_WIDTH = 220;
-var MAP_HEIGHT = 100;
-var MAP_ZOOM = 14;
-
-/**
- * Convert from degrees to radians
- */
-function toRad(angle) {
-  return angle * Math.PI / 180;
-}
-
-/**
- * Get the url for google directions from start to end
- */
-function urlForDirections(start, end) {
-  var template = "http://maps.google.com/?saddr={start_lat},{start_lng}&daddr={end_lat},{end_lng}";
-  return template.replace('{start_lat}', start.latitude)
-    .replace('{start_lng}', start.longitude)
-    .replace('{end_lat}', end.latitude)
-    .replace('{end_lng}', end.longitude)
-}
-
-/**
- * Get url for static map for lat/long
- */
-function urlForMap(latitude, longitude) {
-  var latLng = [latitude, longitude].join(',');
-  var template = 'http://maps.googleapis.com/maps/api/staticmap?center={latLng}' +
-    '&zoom={zoom}&size={width}x{height}&sensor=false&markers=color:red%7C{latLng}';
-  // yes, I know it's inefficient ;-)
-  return template.
-    replace(/{latLng}/g, latLng).
-    replace(/{width}/g, MAP_WIDTH).
-    replace(/{height}/g, MAP_HEIGHT).
-    replace(/{zoom}/g, MAP_ZOOM);
-}
-
-/**
- * http://www.movable-type.co.uk/scripts/latlong.html
- * @param here
- * @param there
- * @return {Number}
- */
-function getDistance(here, there) {
-  var R = 6371; // km
-  var dLat = toRad(there.latitude - here.latitude);
-  var dLon = toRad(there.longitude - here.longitude);
-  var lat1 = toRad(here.latitude);
-  var lat2 = toRad(there.latitude);
-
-  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
-  return Math.round(d * 10) / 10; // 1 decimal place
-}
-
-var auth = {
-  consumerKey: "4ZUyLz2rIvU5cIzCHnnkbA",
-  consumerSecret: "sWo_pi77q5WBYGP5k8pP4Cwgxgw",
-  accessToken: "cFcqGdyz8zpfO3NLTDJwK3vhT2eKLm_7",
-  // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-  // You wouldn't actually want to expose your access token secret like this in a real application.
-  accessTokenSecret: "3qVfLwOMxyBOtRaI4e5gPGcpHf4",
-  serviceProvider: {
-    signatureMethod: "HMAC-SHA1"
-  }
-};
-
-/**
- * Get nearby places from Yelp and update the DOM
- */
-function getAndRenderYelpPlaces(latitude, longitude, accuracy) {
-  var message = {
-    'action': 'http://api.yelp.com/v2/search',
-    'method': 'GET',
-    'parameters': [
-      ['term', 'food'],
-      ['callback', 'cb'],
-      ['oauth_consumer_key', auth.consumerKey],
-      ['oauth_consumer_secret', auth.consumerSecret],
-      ['oauth_token', auth.accessToken],
-      ['oauth_signature_method', 'HMAC-SHA1'],
-      ['category_filter', 'streetvendors,foodstands'],
-      ['sort', SORT_TYPE.distance],
-      //['location', 'San+Francisco'],
-      ['ll', [latitude, longitude, accuracy].join(',')]
-    ]
+  var SORT_TYPE = {
+    distance: 1
   };
 
-  OAuth.setTimestampAndNonce(message);
-  OAuth.SignatureMethod.sign(message, {
-    consumerSecret: auth.consumerSecret,
-    tokenSecret: auth.accessTokenSecret
-  });
+  var MAP_WIDTH = 220;
+  var MAP_HEIGHT = 100;
+  var MAP_ZOOM = 14;
 
-  var parameterMap = OAuth.getParameterMap(message.parameters);
-  parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
+  /**
+   * Convert from degrees to radians
+   */
+  function toRad(angle) {
+    return angle * Math.PI / 180;
+  }
 
-  $.ajax({
-    url: message.action,
-    data: parameterMap,
-    cache: true,
-    dataType: 'jsonp',
-    jsonpCallback: 'cb',
-    success: function (data, textStats, XMLHttpRequest) {
-      console.log('data', data);
+  /**
+   * Get the url for google directions from start to end
+   */
+  function urlForDirections(start, end) {
+    var template = "http://maps.google.com/?saddr={start_lat},{start_lng}&daddr={end_lat},{end_lng}";
+    return template.replace('{start_lat}', start.latitude)
+      .replace('{start_lng}', start.longitude)
+      .replace('{end_lat}', end.latitude)
+      .replace('{end_lng}', end.longitude)
+  }
 
-      var start = {latitude: latitude, longitude: longitude};
+  /**
+   * Get url for static map for lat/long
+   */
+  function urlForMap(latitude, longitude) {
+    var latLng = [latitude, longitude].join(',');
+    var template = 'http://maps.googleapis.com/maps/api/staticmap?center={latLng}' +
+      '&zoom={zoom}&size={width}x{height}&sensor=false&markers=color:red%7C{latLng}';
+    // yes, I know it's inefficient ;-)
+    return template.
+      replace(/{latLng}/g, latLng).
+      replace(/{width}/g, MAP_WIDTH).
+      replace(/{height}/g, MAP_HEIGHT).
+      replace(/{zoom}/g, MAP_ZOOM);
+  }
 
-      $(".businesses").html('');
+  /**
+   * http://www.movable-type.co.uk/scripts/latlong.html
+   */
+  function getDistance(here, there) {
+    var R = 6371; // km
+    var dLat = toRad(there.latitude - here.latitude);
+    var dLon = toRad(there.longitude - here.longitude);
+    var lat1 = toRad(here.latitude);
+    var lat2 = toRad(there.latitude);
 
-      _.each(data.businesses, function (business) {
-        var prettyCategories = _.map(business.categories,
-          function (category) {
-            return category[0];
-          }).join(', ');
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return Math.round(d * 10) / 10; // 1 decimal place
+  }
 
-        var distance = getDistance(start, business.location.coordinate);
+  var auth = {
+    consumerKey: "4ZUyLz2rIvU5cIzCHnnkbA",
+    consumerSecret: "sWo_pi77q5WBYGP5k8pP4Cwgxgw",
+    accessToken: "cFcqGdyz8zpfO3NLTDJwK3vhT2eKLm_7",
+    // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+    // You wouldn't actually want to expose your access token secret like this in a real application.
+    accessTokenSecret: "3qVfLwOMxyBOtRaI4e5gPGcpHf4",
+    serviceProvider: {
+      signatureMethod: "HMAC-SHA1"
+    }
+  };
 
-        var templateData = _.extend({}, business, {
-          pretty_categories: prettyCategories,
-          distance: distance,
-          directions_url: urlForDirections(start, business.location.coordinate),
-          map_url: urlForMap(business.location.coordinate.latitude, business.location.coordinate.longitude)
+  /**
+   * Get nearby places from Yelp and update the DOM
+   */
+  function getAndRenderYelpPlaces(latitude, longitude, accuracy) {
+    var message = {
+      'action': 'http://api.yelp.com/v2/search',
+      'method': 'GET',
+      'parameters': [
+        ['term', 'food'],
+        ['callback', 'cb'],
+        ['oauth_consumer_key', auth.consumerKey],
+        ['oauth_consumer_secret', auth.consumerSecret],
+        ['oauth_token', auth.accessToken],
+        ['oauth_signature_method', 'HMAC-SHA1'],
+        ['category_filter', 'streetvendors,foodstands'],
+        ['sort', SORT_TYPE.distance],
+        ['ll', [latitude, longitude, accuracy].join(',')]
+      ]
+    };
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, {
+      consumerSecret: auth.consumerSecret,
+      tokenSecret: auth.accessTokenSecret
+    });
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
+
+    // TODO: using jsonp so can't attach an error handler. Proxy instead of using jsonp.
+    $.ajax({
+      url: message.action,
+      data: parameterMap,
+      cache: true,
+      dataType: 'jsonp',
+      jsonpCallback: 'cb',
+      success: function (data, textStats, XMLHttpRequest) {
+        var start = {latitude: latitude, longitude: longitude};
+
+        $(".businesses").html('');
+
+        _.each(data.businesses, function (business) {
+          var prettyCategories = _.map(business.categories,
+            function (category) {
+              return category[0];
+            }).join(', ');
+
+          //var neighbourhood = business.location.neighborhoods && business.location.neighborhoods[0];
+          var address = business.location.address && business.location.address[0];
+          //var prettyLocation = _.compact([neighbourhood, address]).join(' - ');
+
+          var distance = getDistance(start, business.location.coordinate);
+
+          var templateData = _.extend({}, business, {
+            pretty_categories: prettyCategories,
+            pretty_location: address,
+            distance: distance,
+            directions_url: urlForDirections(start, business.location.coordinate),
+            map_url: urlForMap(business.location.coordinate.latitude, business.location.coordinate.longitude)
+          });
+
+          $(".businesses").append(ich.tpl_business(templateData))
         });
+      }
+    });
+  }
 
-        $(".businesses").append(ich.tpl_business(templateData))
+  return {
+    init: function () {
+      navigator.geolocation.getCurrentPosition(function (location) {
+        getAndRenderYelpPlaces(location.coords.latitude, location.coords.longitude, location.coords.accuracy);
+      }, function () {
+        // TODO: show a nicer error message/page
+        alert('could not get current location');
       });
     }
-  });
-}
-
-/**
- * Start the application
- */
-function init() {
-  if (!navigator.geolocation && !navigator.geolocation.getCurrentPosition) {
-    // TODO: ugly, show a nicer error page with instructions
-    alert('Error: this app can only be used in a browser that supports geolocation.');
-    return;
   }
 
-  if (!document.addEventListener) {
-    // TODO: not an inherent problem. Should path ICanHaz to work with IE < 9
-    alert('You are using an old version of Internet Explorer. Please use a modern browser.');
-    return;
-  }
+})();
 
-  navigator.geolocation.getCurrentPosition(function (location) {
-    getAndRenderYelpPlaces(location.coords.latitude, location.coords.longitude, location.coords.accuracy);
-  }, function () {
-    // TODO: show a nicer error message/page
-    alert('could not get current location');
-  });
+
+if (!navigator.geolocation && !navigator.geolocation.getCurrentPosition) {
+  // TODO: ugly, show a nicer error page with instructions
+  alert('Error: this app can only be used in a browser that supports geolocation.');
 }
-
-// go!
-$LAB
-  .script('jquery.min.js')
-  .script('oauth.js')
-  .script('sha1.js')
-  .script('ICanHaz.js')
-  .script('underscore.js')
-  .wait(init);
+else if (!document.addEventListener) {
+  // TODO: not an inherent problem. Should path ICanHaz to work with IE < 9
+  alert('You are using an old version of Internet Explorer. Please use a modern browser.');
+}
+else {
+  // load scripts in parallel then start the app
+  $LAB
+    .script('jquery.min.js')
+    .script('oauth.js')
+    .script('sha1.js')
+    .script('ICanHaz.js')
+    .script('underscore.js')
+    .wait(function () {
+      app.init();
+    });
+}
