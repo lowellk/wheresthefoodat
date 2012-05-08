223bc47d36c68e4221fd4b1aec8c931ef7e419f6
Lowell Kirsh <lowellk@gmail.com>
2012-05-08 11:02:21 -0700

    change subtitle; no debug mode in prod

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
+import os, flask
 from werkzeug import SharedDataMiddleware
 from flask import Flask
-import flask
 
-app = Flask(__name__)#, static_folder='static', static_url_path='/')
+app = Flask(__name__)
+
 # http://stackoverflow.com/questions/4239825/static-files-in-flask-robot-txt-sitemap-xml-mod-wsgi
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
     '/': os.path.join(os.path.dirname(__file__), 'static')
@@ -18,5 +18,5 @@ def hello():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    debug = True # TODO: don't deploy in debug mode!
+    debug = os.environ.get('DEBUG', False)
     app.run(host='0.0.0.0', port=port, debug=debug)
